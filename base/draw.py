84a4f18cb448b339e3d99637e80e1a223917666b
Lee-Junhee <junhee.lee002@gmail.com>
2020-04-20 12:45:58 -0400

    nah it ain't the underscore

--- base/draw.py
+++ base/draw.py
@@ -24,15 +24,15 @@ def scanline_convert(polygons, i, screen, zbuffer ):
     try:
         dx0 = (t[0] - b[0]) / (t[1] - b[1])
     except ZeroDivisionError:
-        pass
+        dx0 = 0
     try:
         dx1 = (m[0] - b[0]) / (m[1] - b[1])
     except ZeroDivisionError:
-        pass
+        dx1 = 0
     try:
         dx2 = (t[0] - m[0]) / (t[1] - m[1])
     except ZeroDivisionError:
-        pass
+        dx2 = 0
     color = (105 + randrange(150), 105 + randrange(150), 105 + randrange(150))
     while y <= t[1]:
         draw_line(int(x0), y, 0, int(x1), y, 0, screen, zbuffer, color)
