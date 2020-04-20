417fba35bbb9d333e148296c81b83e0dc3d497e3
Lee-Junhee <junhee.lee002@gmail.com>
2020-04-20 12:41:55 -0400

    removed debug print; fixed list indices to int

--- base/display.py
+++ base/display.py
@@ -30,8 +30,6 @@ def new_zbuffer( width = XRES, height = YRES ):
 def plot( screen, zbuffer, color, x, y, z ):
     newy = YRES - 1 - y
     if ( x >= 0 and x < XRES and newy >= 0 and newy < YRES ):
-        print(newy)
-        print(x)
         screen[newy][x] = color[:]
 
 def clear_screen( screen ):
